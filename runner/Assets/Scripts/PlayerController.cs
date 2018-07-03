66a5c7aa89e3c4621a1839520ddf4cfb4b26d625
Eduardojvr <eduardojvr10@gmail.com>
2018-07-02 21:44:46 -0300

    Adicionado movimentos de salto

---
+++ runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Animator Anime;
+    public Rigidbody2D PlayerRigidbody;
+    public int forceJump;
+    public bool slide;
+
+    public Transform GroundCheck;
+    public bool grounded;
+    public LayerMask wground;
+	// Use this for initialization
+	void Start () {
+        //PlayerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Jump")){
+            PlayerRigidbody.AddForce(new Vector2(0, forceJump));
+        }
+        if(Input.GetButtonDown("Slide")){
+            slide = true;
+        }
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, 0.2f, wground);
+        Anime.SetBool("jump", !grounded);
+        Anime.SetBool("slide", slide);
+	}
+}
